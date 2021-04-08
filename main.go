ed5d3b611fc9462455c25f9cc421236b5f9adfbf
Schmenn <bensholtmann@googlemail.com>
2021-04-08 17:12:01 +0200

    moved the sites map to sites.go

--- main.go
+++ main.go
@@ -5,22 +5,8 @@ import (
 	"strings"
 	"sync"
 
-	"github.com/Schmenn/sherlock/sites"
 	"github.com/fatih/color"
 )
-
-var siteList = map[string]func(username string) bool{
-	"https://github.com/":                       sites.GitHub,
-	"https://www.reddit.com/user/":              sites.Reddit,
-	"https://osu.ppy.sh/users/":                 sites.Osu,
-	"https://gitlab.com/api/v4/users?username=": sites.GitLab,
-	"https://instagram.com/":                    sites.Instagram,
-	"https://tiktok.com/@":                      sites.TikTok,
-	"https://www.chess.com/member/":             sites.Chess,
-	"http://ws2.kik.com/user/":                  sites.Kik,
-	"https://steamcommunity.com/id/":      sites.Steam,
-	"https://twitch.tv/":                        sites.Twitch,
-}
 
 func main() {
 	if len(os.Args) < 2 {
@@ -33,7 +19,7 @@ func main() {
 		wg.Add(1)
 		go func(site string, sitefunc func(username string) bool, name string) {
 			if sitefunc(name) {
-				color.HiGreen("[+] "+ site + name)
+				color.HiGreen("[+] " + site + name)
 			} else {
 				color.HiRed("[-] " + strings.Split(strings.Split(site, "//")[1], "/")[0])
 			}
---
+++ sites.go
@@ -0,0 +1,16 @@
+package main
+
+import "github.com/Schmenn/sherlock/sites"
+
+var siteList = map[string]func(username string) bool{
+	"https://github.com/":                       sites.GitHub,
+	"https://www.reddit.com/user/":              sites.Reddit,
+	"https://osu.ppy.sh/users/":                 sites.Osu,
+	"https://gitlab.com/api/v4/users?username=": sites.GitLab,
+	"https://instagram.com/":                    sites.Instagram,
+	"https://tiktok.com/@":                      sites.TikTok,
+	"https://www.chess.com/member/":             sites.Chess,
+	"http://ws2.kik.com/user/":                  sites.Kik,
+	"https://steamcommunity.com/id/":            sites.Steam,
+	"https://twitch.tv/":                        sites.Twitch,
+}
